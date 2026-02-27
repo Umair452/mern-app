7866126af83d148a1fa4ab293e1c690741772785
Umair452 <arshadu452@gmail.com>
2026-02-27 06:52:09 +0500

    updated index with cors and added .env

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.router.js';
+import cors from 'cors';
 dotenv.config();
 
 
 const app = express();
 app.use(express.json());
+app.use(cors('*'));
+
 
 // app.get('/test', (req, res) => {
 //     res.json({message: 'Backend is working'});
 // });
 
 app.use('/api/products', productRoutes);
-
-
 
 const startServer = async () => {
     await connectDB();
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,57 +1,57 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 const BASE_URL = import.meta.env.VITE_API_URL || '';
 
 export const useProductStore = create((set) => ({
-products: [],
-setProducts: (products) => set({products}),
-createProduct: async (newProduct) => {
-    if (!newProduct.name || !newProduct.price || !newProduct.image) {
-        return {success: false, message: 'All fields are required'};
-    }
+    products: [],
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.price || !newProduct.image) {
+            return { success: false, message: 'All fields are required' };
+        }
 
-    const res = await fetch(`${BASE_URL}/api/products`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newProduct)
-    })
-    const data = await res.json();
-    set((state) => ({products: [...state.products, data.data]}))
-    return {success: true, message: 'Product created successfully'};
-},
+        const res = await fetch(`${BASE_URL}/api/products`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newProduct)
+        })
+        const data = await res.json();
+        set((state) => ({ products: [...state.products, data.data] }))
+        return { success: true, message: 'Product created successfully' };
+    },
 
-fetchProducts: async () => {
-    const res = await fetch(`${BASE_URL}/api/products`);
-    const data = await res.json();
-    set({products: data});
-},
-deleteProduct: async (pid) => {
-    const data = await fetch(`${BASE_URL}/api/products/${pid}`, {
-        method: "DELETE"
-    });
-    const res = await data.json();
-    if(!res.success) {
-        return {success: false, message: res.message};
+    fetchProducts: async () => {
+        const res = await fetch(`${BASE_URL}/api/products`);
+        const data = await res.json();
+        set({ products: data });
+    },
+    deleteProduct: async (pid) => {
+        const data = await fetch(`${BASE_URL}/api/products/${pid}`, {
+            method: "DELETE"
+        });
+        const res = await data.json();
+        if (!res.success) {
+            return { success: false, message: res.message };
+        }
+        set((state) => ({ products: state.products.filter((product) => product._id !== pid) }))
+        return { success: true, message: 'Product deleted successfully' };
+    },
+    updateProduct: async (pid, updatedProduct) => {
+        const res = await fetch(`${BASE_URL}/api/products/${pid}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedProduct)
+        });
+        const data = await res.json();
+        if (!data.success) {
+            return { success: false, message: data.message };
+        }
+        set((state) => ({ products: state.products.map((product) => product._id === pid ? data.data : product) }))
+        return { success: true, message: 'Product updated successfully' };
     }
-    set((state) => ({products: state.products.filter((product) => product._id !== pid)}))
-    return {success: true, message: 'Product deleted successfully'};
-},
-updateProduct: async (pid, updatedProduct) => {
-    const res = await fetch(`${BASE_URL}/api/products/${pid}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(updatedProduct)
-    });
-    const data = await res.json();
-    if(!data.success) {
-        return {success: false, message: data.message};
-    }
-    set((state) => ({products: state.products.map((product) => product._id === pid ? data.data : product)}))
-    return {success: true, message: 'Product updated successfully'};
-}
 
 }));
